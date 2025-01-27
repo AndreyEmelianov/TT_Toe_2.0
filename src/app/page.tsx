4832e27414e60b69c2285218d324af2f70b3ab8a
Andrey Emelianov <621077@mail.ru>
2025-01-27 17:24:15 +0300

    добавил в проект prisma, сделал базовую настройку файла докера, добавил db,  сделал файл сидов для призмы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import { prisma } from '@/shared/lib/db';
 import { Button } from '@/shared/ui/button';
+import { Card, CardTitle } from '@/shared/ui/card';
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
   return (
     <div>
       <Button size="lg" variant="outline">
         Start
       </Button>
+
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
